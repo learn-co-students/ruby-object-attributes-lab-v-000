7fdb6a3c08fd84d45051381ac752130012a71cb3
CodeByLine <yleventhal@ymail.com>
2016-02-23 14:34:58 +0000

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,19 @@
+class Person
+
+  def name=(this_perons_name)
+    @name = this_perons_name
+  end
+
+  def name
+    @name
+  end
+
+  def job=(this_persons_job)
+    @job=this_persons_job
+  end
+
+  def job
+    @job
+  end
+
+end
