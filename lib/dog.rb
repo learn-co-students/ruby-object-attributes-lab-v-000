588bb2b8be43d7562f9e1667c7c2bdc96c55f5ec
kellyayers411 <kellyayers411@gmail.com>
2016-03-25 00:06:23 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,22 @@
+class Dog
+  def name(dog_name)
+    @name=dog_name
+  end
+  def name
+    @name
+  end
+  def name=(name_string)
+    @name=name_string
+  end
+  def breed(dog_breed)
+    @breed=dog_breed
+  end
+  def breed
+    @breed
+  end
+
+  def breed=(breed_string)
+    @breed=breed_string
+  end
+
+end
