1c27ffbbfce605022b62f9b5a900180012615721
yuriyvinnicov <yuriyvinnicov@gmail.com>
2016-03-29 18:14:41 +0000

    Done.

--- lib/person.rb
+++ lib/person.rb
@@ -1,5 +1,10 @@
 class Person
+
   def name(name)
+    @name = name
+  end
+
+  def name=(name)
     @name = name
   end
 
@@ -7,23 +12,18 @@ class Person
     @name
   end
 
-  def name= (new_name)
-    @name = new_name
-  end
-
   def job(job)
-   @job = job
+    @job = job
   end
-
+  
+  def job=(job)
+    @job = job
+  end
+  
   def job
     @job
   end
 
-  def job=(new_job)
-    @job = new_job
-  end
 
 end
-
-sasha = Person.new
 
