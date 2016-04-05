5e41091ddcd250464e791bec51bc32c91d4f3713
bhabig <rfhab2@gmail.com>
2016-04-05 02:00:56 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,17 @@
+class Dog
+  def name=(whats_your_name_boy)
+    @name = whats_your_name_boy
+  end
+
+  def name
+    @name
+  end
+
+  def breed=(what_kinda_dog_areya)
+    @breed = what_kinda_dog_areya
+  end
+
+  def breed
+    @breed
+  end
+end
---
+++ lib/person.rb
@@ -0,0 +1,17 @@
+class Person
+  def name=(who_areya)
+    @name = who_areya
+  end
+
+  def name
+    @name
+  end
+
+  def job=(profession)
+    @job = profession
+  end
+
+  def job
+    @job
+  end
+end
