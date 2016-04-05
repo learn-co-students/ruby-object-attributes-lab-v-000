43e6e99a9fce12b738127f4e124326e48ecd3238
SnowboardTechie <bryan.thompson518@gmail.com>
2016-04-04 18:00:33 -0600

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,17 @@
+class Dog
+  def name=(name_given)
+    @name = name_given
+  end
+
+  def name
+    @name
+  end
+
+  def breed=(breed_given)
+    @breed = breed_given
+  end
+
+  def breed
+    @breed
+  end
+end
---
+++ lib/person.rb
@@ -0,0 +1,17 @@
+class Person
+  def name=(name_given)
+    @name = name_given
+  end
+
+  def name
+    @name
+  end
+
+  def job=(job_given)
+    @job = job_given
+  end
+
+  def job
+    @job
+  end
+end
