bf97ec86c4a0ab4ffa6d29513edbeca91fe52bed
IshmaelKhalid <ishmaelkhalid@icloud.com>
2016-04-12 11:31:25 -0400

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,18 @@
+class Person
+
+  def initialize
+  end
+  def name=(this_name)
+    @name = this_name
+  end
+  def name
+    @name
+  end
+  def job=(this_job)
+    @job = this_job
+  end
+  def job
+    @job
+  end
+
+end
