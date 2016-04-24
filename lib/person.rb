db52a03c20c6a1b1815e366bb9df539120aef4ad
mmelmer <mikemelmer@gmail.com>
2016-04-24 10:26:07 -0400

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,18 @@
+class Person
+  def name=(input)
+    @name = input
+  end
+
+  def name
+    @name
+  end
+  
+  def job=(occupation)
+    @job = occupation
+  end
+
+  def job
+    @job
+  end
+
+end
