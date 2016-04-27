f79f9ca0578bc7e89711ca265abe502e5a8c7d0e
ms94ap <a_papacostas@yahoo.com>
2016-04-27 16:54:57 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,41 @@
+class Dog
+ def names(name)
+   @name = name
+ end
+  def name
+    @name
+  end
+  def name=(new_name)
+    @name = new_name
+  end
+  def breeds(breed)
+    @breed = breed
+  end
+  def breed
+    @breed
+  end
+  def breed=(new_breed)
+    @breed = new_breed
+  end
+end
+
+class Person
+  def persons(name)
+    @name = name
+  end
+  def name
+    @name
+  end
+  def name=(new_name)
+    @name = new_name
+  end
+  def jobs(job)
+    @job = job
+  end
+  def job
+    @job
+  end
+  def job=(new_job)
+    @job = new_job
+  end
+end
