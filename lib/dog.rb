89ccb0eb12e615090ea5a8469c964a1c6a03f872
Eric  Chi <echi8112@gmail.com>
2016-05-06 10:31:34 -0400

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,11 +1,11 @@
 class Dog
 
   def name=(dog_name)
-    @dog_name = dog_name
+    @name = dog_name
   end
 
   def name
-    @dog_name
+    @name
   end
 
   def breed=(breed)
@@ -13,7 +13,7 @@ class Dog
   end
 
   def breed
-    breed
+    @breed
   end
-  
+
 end
