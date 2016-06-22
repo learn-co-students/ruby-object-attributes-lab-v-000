955de81956e4e6b7b7b0100ae1fc530bbeaa26f6
BriOD <briodonoghue@gmail.com>
2016-06-22 19:49:11 -0400

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,15 @@
+class Dog
+  def name=(var)
+    @name=var
+  end
+  def name
+    @name
+  end
+
+  def breed=(var)
+    @breed=var
+  end
+  def breed
+    @breed
+  end
+end
---
+++ lib/person.rb
@@ -0,0 +1,14 @@
+class Person
+  def name=(var)
+    @name=var
+  end
+  def name
+    @name
+  end
+  def job=(var)
+    @job=var
+  end
+  def job
+    @job
+  end
+end
