912f96cf0c485516c1bc27b1fc9985c135cddd38
Rachael Hunsucker <rachael.s.harlow@gmail.com>
2016-06-25 00:57:41 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,17 @@
+class Dog
+  def name=(name_set)
+    @name = name_set
+  end
+
+  def name
+    @name
+  end
+
+  def breed=(breed_set)
+    @breed = breed_set
+  end
+
+  def breed
+    @breed
+  end
+end
---
+++ lib/person.rb
@@ -0,0 +1,17 @@
+class Person
+  def name=(name_set)
+    @name = name_set
+  end
+
+  def name
+    @name
+  end
+
+  def job=(job_set)
+    @job = job_set
+  end
+
+  def job
+    @job
+  end
+end
