89bb278582df25c925b08557e437f79704ab14d3
Scott Wakeman <swakeman@ca.rr.com>
2016-07-27 21:39:49 +0000

    Done.

--- lib/person.rb
+++ lib/person.rb
@@ -1,8 +1,4 @@
 class Person
- #def initialize(name)
-   #@name = name
- #end
-
  def name=(name)
    @name = name
  end
@@ -10,10 +6,6 @@ class Person
  def name
    @name
  end
-
- #def initialize(job)
-   #@job = job
- #end
 
  def job=(job)
    @job = job
