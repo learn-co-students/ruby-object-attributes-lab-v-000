27ed3eb9c41ad9bb1682b24ccec082a2015e85da
Elias Ventura <eliasventura23@gmail.com>
2016-08-08 18:35:11 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,17 +1,3 @@
 class Dog
-  def name=(dogs_name)
-    @name = dogs_name
-  end
-
-  def name
-    @name
-  end
-
-  def breed=(dogs_breed)
-    @breed = dogs_breed
-  end
-
-  def breed
-    @breed
-  end
+  attr_accessor :name, :breed
 end
--- lib/person.rb
+++ lib/person.rb
@@ -1,17 +1,3 @@
 class Person
-  def name=(persons_name)
-    @name = persons_name
-  end
-
-  def name
-    @name
-  end
-
-  def job=(persons_job)
-    @job = persons_job
-  end
-
-  def job
-    @job
-  end
+  attr_accessor :name, :job
 end
