8260b28e37cd973629869448ec2d08f4c55fd984
Chirag Dudhat <cdudhat@gmail.com>
2016-08-19 03:46:36 +0000

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,17 @@
+class Person
+  def name=(name) #Setting a name
+    @name = name
+  end
+
+  def name #Getting or retriving a name
+    @name
+  end
+
+  def job=(job) # Setting the breed
+    @job = job
+  end
+
+  def job # Getting the breed
+    @job
+  end
+end
