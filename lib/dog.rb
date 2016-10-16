6f8718882bf0d66a13fefca3a26eea852d02d467
Mariel Segovia <msegovia94@gmail.com>
2016-10-16 10:45:13 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,18 @@
+class Dog
+  def name
+    #reads name of the dog from instance variable @name
+    @name
+  end
+  def name= dog_name
+    #writes name of dog to an instance variable @name
+    @name = dog_name
+  end
+  def breed
+    #reads breed
+    @breed
+  end
+  def breed= dog_breed
+    #writes breed
+    @breed = dog_breed
+  end
+end
