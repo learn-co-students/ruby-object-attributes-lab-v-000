eafae27853ea01ec09e57196aa2ac175b689c195
Alex Glover <alexanderbglover@gmail.com>
2016-12-15 20:35:57 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,39 @@
+class Dog
+  def name=(dogs_name)
+    @name = dogs_name
+  end
+
+  def name
+    @name
+  end
+
+  def breed=(dogs_breed)
+    @breed = dogs_breed
+  end
+
+  def breed
+    @breed
+  end
+end
+
+fido = Dog.new
+fido.name = "Fido"
+fido.breed = "Beagle"
+
+class Person
+  def name=(persons_name)
+    @name=persons_name
+  end
+
+  def name
+    @name
+  end
+
+  def job=(persons_job)
+    @job=persons_job
+  end
+
+  def job
+    @job
+  end
+end
