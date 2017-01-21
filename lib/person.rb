3cabbc22cf90b9fd26be1d90d59b50ed4ad77bd9
Trai Compton <tlcompton2546@outlook.com>
2017-01-21 19:04:49 +0000

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,17 @@
+class Person
+  def name=(person_name)
+    @name = person_name
+  end
+
+  def name
+    @name
+  end
+
+  def job=(job_role)
+    @job = job_role
+  end
+
+  def job
+    @job
+  end
+end
