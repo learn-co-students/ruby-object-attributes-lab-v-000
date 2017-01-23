49047c4bce38aa101fd29ef4e381866ec4594331
Carlos Quinones <cquinones100@gmail.com>
2017-01-23 13:53:14 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,45 @@
+class Dog
+
+  def name=(new_name)
+    @name = new_name
+  end
+
+  def name
+    @name
+  end
+
+  def breed=(new_breed)
+    @breed = new_breed
+  end
+
+  def breed
+    @breed
+  end
+end
+
+class Person
+
+  def name=(new_name)
+    @name = new_name
+  end
+
+  def name
+    @name
+  end
+
+  def job=(new_job)
+    @job = new_job
+  end
+
+  def job
+    @job
+  end
+end
+
+fido = Dog.new
+fido.name = "Fido"
+fido.breed = "Beagle"
+
+beyonce = Person.new
+beyonce.name = "Beyonce"
+beyonce.job = "Singer"
