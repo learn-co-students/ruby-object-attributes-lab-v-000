ca94212b76485f300c75f4fe05fcf93610f2014e
Neil Ryan <neilmarkoliverryan@gmail.com>
2017-01-27 04:33:23 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,15 +1,16 @@
+
 class Dog
 
-  def name=(dog_name)
-    @name = dog_name
+  def name=(name)
+    @name = name
   end
 
   def name
     @name
   end
 
-  def breed=(dog_breed)
-    @breed = dog_breed
+  def breed=(breed)
+    @breed = breed
   end
 
   def breed
