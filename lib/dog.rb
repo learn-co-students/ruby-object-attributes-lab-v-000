68dcd94e76d11c7b2ec33eb7c2820923f3f19d44
Jenna Francis <jef399@nyu.edu>
2017-02-02 01:12:16 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,27 @@
+class Dog
+
+  def new(name)
+    @name = name
+  end
+
+  def name
+    @name
+  end
+
+  def name=(new_name)
+    @name = new_name
+  end
+
+  def new(breed)
+    @breed = breed
+  end
+
+  def breed
+    @breed
+  end
+
+  def breed=(new_breed)
+    @breed = new_breed
+  end
+  
+end
