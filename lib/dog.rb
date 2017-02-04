7c407e55a553b4ce4d566917b2385a07105066d9
Michael O'Connell <michaeldavidoconnell@gmail.com>
2017-02-04 18:31:20 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,17 @@
+class Dog
+  def name
+    @name
+  end
+
+  def name=(the_dogs_name)
+    @name = the_dogs_name
+  end
+  def breed
+    @breed
+  end
+
+  def breed=(the_dogs_breed)
+    @breed = the_dogs_breed
+  end
+
+end
