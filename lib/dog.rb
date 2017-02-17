3bd8b38c571554c30f8b4d70c501d6ce3305f2df
John Ziegner <jziegner23@gmail.com>
2017-02-17 21:36:45 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,32 @@
+class Dog
+
+
+def name(name_of_dog)
+  @name = name_of_dog
+
+end
+
+def name
+  @name
+
+end
+
+def name=(new_name)
+  @name = new_name
+end
+
+def breed(breed_of_dog)
+  @breed = breed_of_dog
+
+end
+
+def breed
+  @breed
+
+end
+
+def breed=(new_breed)
+  @breed = new_breed
+end
+
+end
