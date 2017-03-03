4bb99abca3d6897e6fe5c4d94090e81845773ef6
Rachel Walwood <walwoodr@gmail.com>
2017-03-03 21:51:00 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,26 @@
+class Dog
+  # def initialize(name, breed)
+  #   @name = name
+  #   @breed = breed
+  # end
+
+  #setter for name
+  def name=(name)
+    @name = name
+  end
+
+  #setter for breed
+  def breed=(breed)
+    @breed = breed
+  end
+
+  #getter for name
+  def name
+    @name
+  end
+
+  #getter for breed
+  def breed
+    @breed
+  end
+end
