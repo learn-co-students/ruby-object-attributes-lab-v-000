51ab827e351f9dfd1ff32347925bf05e4cb19cab
jordan surgick <jordansurgick@aol.com>
2017-03-30 00:14:48 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,24 @@
+class Dog
+  def name=(name)
+    @name=name
+  end
+
+def name
+    @name
+  end
+
+fido = Dog.new
+fido.name = "Fido"
+puts fido.name
+
+def breed=(breed)
+  @breed=breed
+end
+def breed
+  @breed
+    end
+
+snoopy = Dog.new
+snoopy.breed = "Beagle"
+
+end
---
+++ lib/person.rb
@@ -0,0 +1,24 @@
+class Person
+  def name=(name)
+    @name=name
+  end
+
+  def name
+    @name
+  end
+
+beyonce= Person.new
+beyonce.name= "Beyonce"
+puts beyonce.name
+
+def job=(job)
+  @job=job
+end
+def job
+  @job
+    end
+
+singer= Person.new
+singer.job = "Singer"
+
+end
