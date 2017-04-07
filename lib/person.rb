e59209e7220a45a33c11d53220fe2b0d11e17247
Sam Marshall <samuel.r.marshall@gmail.com>
2017-04-07 03:05:54 +0000

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,24 @@
+class Person
+  def get_name(name)
+    @name = name
+  end
+
+  def name
+    @name
+  end
+
+  def name=(new_name)
+    @name = new_name
+  end
+  def get_job(job)
+    @breed = breed
+  end
+
+  def job
+    @job
+  end
+
+  def job=(new_job)
+    @job = new_job
+  end
+end
