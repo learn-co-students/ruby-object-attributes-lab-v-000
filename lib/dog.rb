1c2ccca8df62ebb6e2403a83633def01b79ee747
Luther Meadows <therealmsesqui@gmail.com>
2017-04-10 20:21:54 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,21 @@
+class Dog
+
+  def name=(fido)
+    @name = fido
+  end
+
+  def name
+    @name
+  end
+end
+
+class Dog
+
+  def breed=(beagle)
+    @breed = beagle
+  end
+
+  def breed
+    @breed
+  end
+end
