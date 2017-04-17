b548613e62f68f7824cf618f98ddc05e7b9f51e0
Brian Zoebisch <zoebisch@gmail.com>
2017-04-17 23:13:55 +0000

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,19 @@
+class Person
+
+  def name
+    @name
+  end
+
+  def name=(persons_name)
+    @name = persons_name
+  end
+
+  def job
+    @job
+  end
+
+  def job=(occupation)
+    @job = occupation
+  end
+  
+end
