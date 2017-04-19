fca263f535eed49bce2a0d639859b6a7c1d2c8cb
Tanj McMeans <tanjmcmeans01@gmail.com>
2017-04-19 19:50:18 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,19 @@
+class Dog
+
+  def name
+    @name
+  end
+
+  def name=(dog_name)
+    @name = dog_name
+  end
+
+  def breed
+    @breed #spec to instance of obj
+  end
+
+  def breed=(dog_breed)
+    @breed = dog_breed
+  end 
+
+end
