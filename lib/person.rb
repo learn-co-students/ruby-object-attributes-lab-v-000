5fb4933b150426b50fd375c1d1f6175b5aed3be1
Susan Fowler <infopro94@gmail.com>
2017-04-19 21:55:37 +0000

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,18 @@
+class Person
+  def name=(persons_name)
+  @name = persons_name
+  end
+
+  def name
+    @name
+  end
+
+    def job=(their_job)
+    @job = their_job
+  end
+
+  def job
+    @job
+  end
+
+end
