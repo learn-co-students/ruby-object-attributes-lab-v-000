27a42cac7a6eeb94c6ddec81fb8e8b51895c4076
Jeremy Emata <emata@rapidratings.com>
2017-05-01 19:26:29 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,19 @@
+class Dog
+
+  def name=(entered_name)
+    @name = entered_name
+  end 
+
+  def name
+    @name
+  end
+
+  def breed=(entered_breed)
+    @breed = entered_breed
+  end
+
+  def breed
+    @breed
+  end
+  
+end
