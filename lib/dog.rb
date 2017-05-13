f09d5556dcbc8f1251f1611f1c603b22ecd4f4ea
Byungwook Yoon <byung90@gmail.com>
2017-05-13 19:29:54 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,20 @@
+class Dog
+  #setters
+  def name=(new_name)
+    @name = new_name
+  end
+
+  def breed=(breed_name)
+    @breed = breed_name
+  end
+
+  #getters
+  def name
+    @name
+  end
+
+  def breed
+    @breed
+  end
+
+end
