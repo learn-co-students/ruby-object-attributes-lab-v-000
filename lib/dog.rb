8729a01ef55f0a6a3a4e0ec7e9e56d389b29e2b8
Nicholas D'Amico <nickalan82@icloud.com>
2017-06-05 22:24:05 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,19 @@
+class Dog
+
+  # Getter Method
+  def name
+    @name
+  end
+
+  def breed
+    @breed
+  end
+  # Setter Method
+  def name=(name)
+    @name = name
+  end
+
+  def breed=(breed)
+    @breed = breed
+  end
+end
---
+++ lib/person.rb
@@ -0,0 +1,18 @@
+class Person
+  # Getter Method
+  def name
+    @name
+  end
+
+  def job
+    @job
+  end
+  # Setter Method
+  def name=(name)
+    @name = name
+  end
+
+  def job=(job)
+    @job = job
+  end
+end
