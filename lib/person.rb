e3988852f51247a033a31a1bcb18dca3d9235e6a
Desmond Drakes <desmond.drakes@gmail.com>
2017-06-20 18:48:19 +0000

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,26 @@
+class Person
+
+def initalize(name)
+    @name = name
+end
+
+def name
+  @name
+end
+
+def name=(new_name)
+  @name = new_name
+end
+
+  def initalize(job)
+    @job = job
+  end
+
+  def job
+    @job
+  end
+
+  def job=(job_name)
+    @job = job_name
+  end
+end
