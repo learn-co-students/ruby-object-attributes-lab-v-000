1dae736a9833d181c76de42934047e7feef3306e
Sophia H <shhubeny@gmail.com>
2017-07-03 17:00:01 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,20 @@
+class Dog
+  def bark
+    puts "Woof!"
+  end
+  def sit
+    puts "The Dog is sitting"
+  end
+  def name=(new_name)
+    @name = new_name
+  end
+  def name
+    @name
+  end
+  def breed=(new_breed)
+    @breed = new_breed
+  end
+  def breed
+    @breed
+  end
+end
