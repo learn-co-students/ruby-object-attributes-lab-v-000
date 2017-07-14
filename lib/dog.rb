18d9514d31a8f0a6d41d368d81a9de3ce973a751
Amanda Wehrman <arwehrman@gmail.com>
2017-07-14 22:55:08 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,26 @@
+class Dog
+#name
+  def name=(name)
+    @name = name
+  end
+
+  def name
+    @name
+  end
+
+  def name=(name)
+    @name = name
+  end
+#breed
+  def breed=(breed)
+    @breed = breed
+  end
+
+  def breed
+    @breed
+  end
+
+  def breed=(name)
+    @breed = name
+  end
+end
