e521721deda2594e8a8a95c1a150509d37ad18c5
Isabel Vazquez <12ivazquez@gmail.com>
2017-07-16 19:36:53 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,17 @@
+class Dog
+  def name=(name) #writes the name of the dog to an instance variable @name
+    @name = name
+  end
+
+  def name #reads the name of the dog from an instance variable @name
+    @name
+  end
+
+  def breed=(breed) #writes the breed of the dog to an instance variable @breed
+    @breed = breed
+  end
+
+  def breed #reads the breed of the dog from an instance variable @breed
+    @breed
+  end
+end
