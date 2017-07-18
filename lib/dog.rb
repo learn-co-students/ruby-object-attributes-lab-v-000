514e0b8e3af6c8c73ea74aea13691d73eeef862a
Sofia Candiani <sofia.candiani123@gmail.com>
2017-07-18 21:49:02 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,14 @@
+class Dog
+  def name=(name) #writes the name of the dog to an instance variable @name
+    @name = name
+  end
+  def name
+    @name
+  end
+  def breed=(breed)
+    @breed = breed
+  end
+  def breed
+    @breed
+  end
+end
