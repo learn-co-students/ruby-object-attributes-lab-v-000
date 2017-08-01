a525cf818aab65c343b79cbe7fd28be58688652c
Claribel Aguilar <claribel.aguilarwhyte@gmail.com>
2017-08-01 13:04:11 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,25 @@
+class Dog
+def initalize(name)
+  @name = name
+  end
+
+def name
+    @name
+  end
+
+def name=(new_name)
+  @name= new_name
+  end
+
+def initalize(breed)
+  @breed = breed
+  end
+
+def breed
+    @breed
+  end
+
+def breed=(new_breed)
+  @breed= new_breed
+  end
+end
