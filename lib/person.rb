dcc3dd4bc7a625087d4f125ab8a2344b6aac452b
Jennifer Wilson <jennwilson55@gmail.com>
2017-08-24 01:05:53 +0000

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,21 @@
+class Person
+  def name= full_name
+    @name = full_name
+  end
+
+  def name
+    @name
+  end
+
+  def job= job
+    @job = job
+  end
+
+  def job
+    @job
+  end
+end
+
+beyonce = Person.new
+beyonce.name = "Beyonce"
+beyonce.job = "Singer"
