fe4670a5d407341bdd0ad7d3eb15f7bb6c265f7b
Fiona Lapham <fionadlapham@gmail.com>
2017-08-27 13:56:54 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,14 @@
+class Dog
+  def name= (name)  #I don't understand when the initilize method enters into the picture. 
+    @name = name
+  end
+  def name
+    @name
+  end
+  def breed= (breed)
+    @breed = breed
+  end
+  def breed
+    @breed
+  end
+end
---
+++ lib/person.rb
@@ -0,0 +1,17 @@
+class Person
+  def name= (name) #still not understanding this concept of writer/readers...go back and watch video again.
+    @name = name
+  end
+
+  def name
+    @name
+  end
+
+  def job= (job)
+    @job = job
+  end
+
+  def job
+    @job
+  end
+end
