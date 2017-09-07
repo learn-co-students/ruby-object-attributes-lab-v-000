879e490a44cbdf6468fe77360f20034a9c9a8dd8
Dani Schuhman <dani8439@aol.com>
2017-09-07 19:56:02 +0000

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,19 @@
+class Person
+
+  def name=(a_name)
+    @name = a_name
+  end
+
+  def name 
+    @name
+  end
+
+  def job=(occupation)
+    @job = occupation
+  end
+
+  def job
+    @job
+  end
+  
+end
