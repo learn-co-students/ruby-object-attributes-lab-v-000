0b6929ff83e37c65c1f7e5fe4f253fbe12d9a411
Andrea Nasuto <andrea.nasuto@gmail.com>
2017-09-07 23:18:23 +0000

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,25 @@
+class Person
+  def initiate(a_name)
+    name = a_name
+  end
+
+  def name
+    @name
+  end
+
+  def name=(new_name)
+    @name = new_name
+  end
+
+  def initiate(a_job)
+    job = a_job
+  end
+
+  def job
+    @job
+  end
+
+  def job=(new_job)
+    @job = new_job
+  end
+end
