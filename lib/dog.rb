ab7df3f9243ea4f0c454cf04c04d87555acd0278
Ben Blackman <bblkmn5@gmail.com>
2017-09-14 04:57:37 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,23 @@
+class Dog
+  def initialize
+    @name = name
+    @breed = breed
+  end
+
+  def name=(the_name)
+    @name = the_name
+  end
+
+  def name
+    @name
+  end
+
+  def breed=(the_breed)
+    @breed = the_breed
+  end
+
+  def breed
+    @breed
+  end
+
+end
---
+++ lib/person.rb
@@ -0,0 +1,23 @@
+class Person
+  def initialize
+    @name = name
+    @job = job
+  end
+
+  def name=(the_name)
+    @name = the_name
+  end
+
+  def name
+    @name
+  end
+
+  def job=(the_job)
+    @job = the_job
+  end
+
+  def job
+    @job
+  end
+
+end
