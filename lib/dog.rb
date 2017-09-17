91e743724c397ccef9f0c88c8effb7faafd552e4
Juliet <jktallmer2@gmail.com>
2017-09-17 20:51:56 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,19 @@
+class Dog
+  #setter
+  def name=(dog_name)
+    @name = dog_name
+  end
+
+  #getter
+  def name
+    @name
+  end
+
+  def breed=(dog_breed)
+    @breed = dog_breed
+  end
+
+  def breed
+    @breed
+  end
+end
