5ed9214eba963152a241a1f5511046951d0370c4
Amanda White <emmalinerosewhite@gmail.com>
2017-09-18 12:41:54 +0000

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,18 @@
+class Person
+  def name=(persons_name)
+    @name = persons_name
+  end
+
+  def name
+    @name
+  end
+
+  def job=(jobby_job)
+    @job = jobby_job
+  end
+
+  def job
+    @job
+  end
+
+end
