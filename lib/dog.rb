39b06c414b90366a30fc580b33a8bad15220e954
Alyssa Watson <alyssa.s.watson@gmail.com>
2017-10-02 01:25:20 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,15 @@
+class Dog
+
+   def name=(dogname)
+     @name = dogname
+   end
+   def name
+     @name
+   end
+   def breed=(breedname)
+     @breed = breedname
+   end
+   def breed
+     @breed
+   end
+ end
---
+++ lib/person.rb
@@ -0,0 +1,15 @@
+class Person
+
+   def name=(personname)
+     @name = personname
+   end
+   def name
+     @name
+   end
+   def job=(jobname)
+     @job = jobname
+   end
+   def job
+     @job
+   end
+ end
