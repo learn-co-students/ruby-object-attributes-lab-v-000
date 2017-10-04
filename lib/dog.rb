bc558fc000f4b7f7a2824f97d7deec66325264d3
Lekan Nicholson <lekan.nicholson@gmail.com>
2017-10-04 23:09:13 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,14 @@
+class Dog
+  def name=(dog_name)
+    @name = dog_name
+  end
+  def name
+    name = @name
+  end
+  def breed=(breed_type)
+    @breed = breed_type
+  end
+  def breed
+    breed = @breed
+  end
+end
---
+++ lib/person.rb
@@ -0,0 +1,14 @@
+class Person
+  def name=(persons_name)
+    @name = persons_name
+  end
+  def name
+    name = @name
+  end
+  def job=(job_name)
+    @job = job_name
+  end
+  def job
+    job = @job
+  end
+end
