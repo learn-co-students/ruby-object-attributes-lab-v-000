2910138e316da45fb92b50bb737216f721b60770
zolkocarbon <eathena@hotmail.com>
2017-10-05 12:36:56 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,6 +2,7 @@ class Dog
   def name=(dog_name)
     @name = dog_name
   end
+
   def name
     @name
   end
@@ -9,6 +10,7 @@ class Dog
   def breed=(dog_breed)
     @breed = dog_breed
   end
+
   def breed
     @breed
   end
