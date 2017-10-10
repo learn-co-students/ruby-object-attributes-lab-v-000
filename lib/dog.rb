781ab30345097cfc50dfdcbdd65049fb6ef5335c
amckean12 <amckean@me.com>
2017-10-10 18:34:40 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,16 @@
+class Dog
+  #takes in an arguement of dog name and will set that equal to a variable
+  def name=(dog_name)
+    @name= dog_name
+  end
+  #method will report the dog name
+  def name
+    @name
+  end
+  def breed=(dog_breed)
+    @breed = dog_breed
+  end
+  def breed
+    @breed
+  end
+end
