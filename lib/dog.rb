f68bf313f3ba3ddd58f7e64690d52c3e1f70a5bf
Joshua Tan <jtsarvida@gmail.com>
2017-10-12 18:03:37 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,17 @@
+class Dog
+  def name=(dogs_name)
+    @name = dogs_name
+  end
+
+  def name
+    return @name
+  end
+
+  def breed=(dogs_breed)
+    @breed = dogs_breed
+  end
+
+  def breed
+    return @breed
+  end
+end
