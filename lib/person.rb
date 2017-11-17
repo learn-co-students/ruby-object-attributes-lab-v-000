f89568b974f36545e7527ee3037d24ce14bb1b35
Jenelynrose Hopkins <jenelyn.hopkins@gmail.com>
2017-11-17 17:26:31 +0000

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,19 @@
+class Person
+ def name=(beyonce)
+ @name = beyonce
+ end
+
+ def name
+   @name
+ end
+   def job=(singer)
+     @job = singer
+end
+     def job
+       @job
+     end
+ end
+
+ beyonce = Person.new
+ beyonce.name = "beyonce"
+ beyonce.job = "singer"
