9ff7a756d7c0d24feb2d02ece342a8db1d7cdc32
Phoebe Lau <phoebelau7@gmail.com>
2017-11-18 02:04:15 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,18 @@
+class Dog
+def name=(dogs_newname) #setter method sets the dogs name
+  @name = dogs_newname
+end
+
+def name #getter method reports the dogs name
+  @name
+end
+
+def breed=(dogs_breed) #setter method
+  @breed = dogs_breed
+end
+
+def breed
+  @breed
+end
+
+end
