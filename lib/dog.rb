74c1c8413ca685778525ef567832fffa6561d4c8
Kriti Rai <kriti.rai@gmail.com>
2017-11-28 04:21:17 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,14 @@
+class Dog
+  def name=(name_of_the_dog)
+  @name = name_of_the_dog
+  end
+  def name
+    @name
+  end
+  def breed=(breed_name)
+    @breed = breed_name
+  end
+  def breed
+    @breed
+  end
+end
