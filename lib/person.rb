1d5a2b7f17d9d3277510aaa6a011b0c5347f446a
Aubrey Neimeier <neimeier.1@osu.edu>
2017-12-04 22:21:47 +0000

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,22 @@
+class Person
+
+#setter method assigns a instance variable to the argument pass to it, name
+  def name=(name)
+    @name = name
+  end
+
+#getter method reads or writes the instance variable (@name).
+  def name
+    @name
+  end
+
+#setter method assigns a instance variable to the argument pass to it, job
+  def job=(job)
+    @job = job
+  end
+#getter method reads or writes the instance variable (@job).
+  def job
+    @job
+  end
+
+end
