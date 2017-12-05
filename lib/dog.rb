7fc8b2f5d391b452a117dee4c9cb6a252068d2a8
Daniel Nunez <daniel.nunez.nyc@gmail.com>
2017-12-05 03:30:39 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,39 @@
+class Dog
+  def intialize(name, breed)
+    @name = name
+  end
+
+  def name=(name)
+    @name = name
+  end
+  def name
+    @name
+  end
+
+  def breed=(breed)
+    @breed = breed
+  end
+  def breed
+    @breed
+  end
+end
+
+class Person
+  def intialize(name, job)
+    @name = name
+  end
+
+  def name=(name)
+    @name = name
+  end
+  def name
+    @name
+  end
+
+  def job=(job)
+    @job = job
+  end
+  def job
+    @job
+  end
+end
