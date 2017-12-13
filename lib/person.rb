26c6425633de9f655f59a3ca46d59e2cfb6e9a1c
Alavia Yahya <alavia.yahya@gmail.com>
2017-12-13 09:16:18 +0000

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,14 @@
+class Person
+  def name= persons_name #setter/writer
+    @name = persons_name
+  end
+  def name #getter/reader
+    @name
+  end
+  def job= persons_job #setter/writer
+    @job = persons_job
+  end
+  def job #getter/reader
+    @job
+  end
+end
