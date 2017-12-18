1cda78c508b7810aa709eb9b870e6f1d538fe20e
Fumiyo Bookstein <miyoboo4@gmail.com>
2017-12-18 22:48:29 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,18 @@
+class Dog
+  def name=(dog_name)
+    fido = Dog.new
+    @name= "Fido"
+  end
+
+  def name
+    @name
+  end
+
+  def breed=(dog_breed)
+    dog_breed = "Beagle"
+    @breed = "Beagle"
+  end
+  def breed
+    @breed
+  end
+end
