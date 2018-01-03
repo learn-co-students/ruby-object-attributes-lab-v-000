41a712e83494db0a983b59c398e7e722553cb5d0
Kristin Reddington <kristinreddington@gmail.com>
2018-01-03 02:36:35 +0000

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,18 @@
+class Person
+  def name
+    @name
+  end
+
+def name=(person_new_name)
+  @name = person_new_name
+end
+
+def job
+  @job
+end
+
+def job=(person_new_job)
+  @job = person_new_job
+end
+
+end
