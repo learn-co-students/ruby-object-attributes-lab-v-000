e6e44082f869d647942d499194ac376f47c73072
Kim Gonzales <kimgo589@gmail.com>
2018-01-10 15:36:32 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,19 @@
+class Dog
+
+  def name=(name_of_dog)
+    @name = name_of_dog
+  end
+
+  def name
+    @name
+  end
+
+  def breed= (breed_of_dog)
+    @breed = breed_of_dog
+  end
+
+  def breed
+    @breed
+  end
+
+end
