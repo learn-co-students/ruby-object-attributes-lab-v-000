d23b1afe0fcb9b4663b82d6d818b37d46f697430
Hiroki Kato <hkato@sas.upenn.edu>
2018-02-02 20:16:32 +0000

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,18 @@
+class Person
+
+  def name= (persons_name)
+    @name = persons_name
+  end
+
+  def name
+    @name
+  end
+
+  def job= (occupation)
+    @job = occupation
+  end
+
+  def job
+    @job
+  end
+end
