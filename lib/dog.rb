0f5e1c355fd97a1bc44ba270b574c2fb8115c1cc
Laura Messa Travin <laura.travin@gmail.com>
2018-02-05 20:23:22 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,18 @@
+class Dog
+   def initialize() #no 
+       @name
+   end
+   def name=(new_name) #setter of name
+     @name = new_name
+   end
+   def name #getter of name
+     @name
+   end
+   def breed=(breed) #setter of breed
+     @breed = breed
+   end
+   def breed #setter of breed
+     @breed
+   end
+
+end
