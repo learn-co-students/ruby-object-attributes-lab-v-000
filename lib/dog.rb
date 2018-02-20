d5ab8cd1684ba49cd528eebb4bbde0e34cfe62eb
Andrew Kairys <andrewkairys@gmail.com>
2018-02-20 17:50:59 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,26 @@
+class Dog
+
+def name(name)
+  @name = name
+end
+
+def name
+  @name
+end
+
+def name= (dog_name)
+  @name = dog_name
+end
+
+def breed(breed)
+  @breed = breed
+end
+
+def breed
+  @breed
+end
+
+def breed= (breed_name)
+  @breed = breed_name
+end
+end
---
+++ lib/person.rb
@@ -0,0 +1,27 @@
+class Person
+
+  def name(name)
+    @name = name
+  end
+
+  def name
+    @name
+  end
+
+  def name= (person_name)
+    @name = person_name
+  end
+
+ def job(job)
+   @job = job
+ end
+
+ def job
+   @job
+ end
+
+ def job= (job_name)
+   @job = job_name
+ end
+
+end
