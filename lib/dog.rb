23efe861768ac1613d8186f4e669a259de23980a
Madeline Stark <madeline.stark1@gmail.com>
2018-02-27 23:54:20 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,14 @@
+class Dog
+  def name=(dogs_name)
+    @name = dogs_name
+  end
+  def name
+    @name
+  end
+  def breed=(dogs_bread)
+    @breed = dogs_bread
+  end
+  def breed
+    @breed
+  end
+end
