c323be9cc8f46710333bcb39c0ec9a376ba66f01
David Markel <davidgmarkel@gmail.com>
2018-03-07 16:48:15 +0000

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,19 @@
+class Person
+
+  def name
+    @name
+  end
+
+  def name=(their_name)
+    @name = their_name
+  end
+
+  def job
+    @job
+  end
+
+  def job=(their_job)
+    @job = their_job
+  end
+
+end
