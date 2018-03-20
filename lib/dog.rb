f0733f281c98ab452fb9ca29bf7c35b3d98a3a21
Diogo Ide <diogoide@gmail.com>
2018-03-20 00:17:22 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,17 +1,19 @@
 class Dog
-  def name
-    @name 
+
+  def name=(name)
+    @name= name
   end
 
-  def name=(new_name)
-    @name= new_name
+  def name
+    @name
+  end
+
+
+  def breed=(breed)
+    @breed= breed
   end
 
   def breed
     @breed
-  end
-
-  def breed=(new_breed)
-    @breed= new_breed
   end
 end
