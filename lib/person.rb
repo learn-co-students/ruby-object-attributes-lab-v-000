98587e454d7730de3d896b48076236e0ed177465
Eric Fiorani <efiorani305@gmail.com>
2018-03-21 13:58:12 +0000

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,14 @@
+class Person
+  def name=(full_name)
+    @name = full_name
+  end
+  def name
+    @name
+  end
+  def job=(occupation)
+    @job = occupation
+  end
+  def job
+    @job
+  end
+end
