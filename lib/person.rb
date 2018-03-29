027ff08406ad0e625d9d649492a7ec23163973e1
Josh Stegmeier <josh@steg.me>
2018-03-29 22:07:59 +0000

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,22 @@
+class Person
+  # def initialize(initial_name, initial_job)
+  #   @name = initial_name
+  #   @job = initial_job
+  # end
+
+  def name
+    @name
+  end
+
+  def name=(new_name)
+    @name = new_name
+  end
+
+  def job
+    @job
+  end
+
+  def job=(new_job)
+    @job = new_job
+  end
+end
