db14a151200187a4faf59e446d82a7119adf552b
Alejandro Sabogal <asabogal.git@gmail.com>
2018-03-30 00:46:23 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,19 @@
+class Dog
+
+  def name= (write_name)
+    @name = write_name
+  end
+
+  def name
+    @name
+  end
+
+  def breed= (write_breed)
+    @breed = write_breed
+  end
+
+  def breed
+    @breed
+  end
+
+end
---
+++ lib/person.rb
@@ -0,0 +1,18 @@
+class Person
+  def name= (write_name)
+    @name = write_name
+  end
+
+  def name
+    @name
+  end
+
+  def job= (write_job)
+    @job = write_job
+  end
+
+  def job
+    @job
+  end
+
+end
