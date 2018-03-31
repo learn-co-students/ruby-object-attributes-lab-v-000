fd3afdf2c61f36755e1a09afffd780f90b9d47b6
Karen O <k.ortiz026@gmail.com>
2018-03-31 19:07:56 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,18 @@
+class Dog
+  #Sets dog's name
+  def name=(dog_name)
+    @name = dog_name
+  end
+  #Gets dog's name
+  def name
+    @name
+  end
+  #Sets dog's breed
+  def breed=(dog_breed)
+    @breed = dog_breed
+  end
+  #Gets dog's breed
+  def breed
+    @breed
+  end
+end
