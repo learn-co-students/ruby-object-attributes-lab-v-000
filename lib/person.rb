75143d3023147586e1632c3bc5d8ce2e02a4047e
Carrie Savinell <carrielane89@gmail.com>
2018-04-06 15:59:03 +0000

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,20 @@
+class Person 
+  
+  def name=(dummie_name)
+    @name = dummie_name
+  end   
+  
+  def name 
+    "#{@name}" 
+  end   
+
+  def job=(my_job)
+    @job = my_job
+  end
+  
+  def job
+    @job
+  end   
+
+end   
+
