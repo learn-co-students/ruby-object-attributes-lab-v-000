fa28efc14275405d24c23134d452f1673c076839
Ashley Schultz <ashleyeschultz@gmail.com>
2018-04-12 01:36:25 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,15 @@
+class Dog 
+  def breed=dog_breed
+    @breed = dog_breed
+  end
+  def breed
+    @breed
+  end 
+  
+  def name=dog_name
+    @name = dog_name
+  end
+  def name
+    @name
+  end 
+end
