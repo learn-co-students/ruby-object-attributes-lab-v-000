2622d365192cfefa464ac5bf6c9cb22731b1149a
cindych419@gmail.com <cindych419@gmail.com>
2018-04-25 23:33:42 +0000

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,29 @@
+class Person
+  def teaching(name)
+    @name = name
+  end
+  def name
+    @name
+  end
+  def name=(new_name)
+    @name = new_name
+  end
+  def teaching(name)
+    @name = name
+  end
+  def name
+    @name
+  end
+  def name=(new_name)
+    @name = new_name
+  end
+  def teacing(job)
+    @job = job
+  end
+  def job
+    @job
+  end
+  def job=(new_job)
+    @job = new_job
+  end
+end
