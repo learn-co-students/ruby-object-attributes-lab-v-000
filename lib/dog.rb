7fdf99827f0c61d16e4d1a1baf9edd4d5d8351f1
Will Lee <will.i.lee@outlook.com>
2018-05-08 21:36:19 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,16 @@
+class Dog
+
+    def name=(dog_name)
+      @name = dog_name
+    end
+    def name()
+      dog_name = @name
+    end
+    def breed=(breed_name)
+      @breed = breed_name
+    end
+    def breed()
+      breed = @breed
+    end
+
+end
---
+++ lib/person.rb
@@ -0,0 +1,14 @@
+class Person
+  def name=(person_name)
+    @name = person_name
+  end
+  def name()
+    person_name = @name
+  end
+  def job=(job_name)
+    @job = job_name
+  end
+  def job()
+    job = @job
+  end
+end
