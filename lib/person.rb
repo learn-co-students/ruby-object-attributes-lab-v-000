92e67ca6aed60a3c2a3ed304c6cca2d71de8bf8e
Richard Sanchez <richard.charles.s@gmail.com>
2018-06-11 17:58:00 +0000

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,23 @@
+class Person
+  def intitialize(name, job)
+    @name = name
+    @job = job
+  end
+
+  def name
+    @name
+  end
+
+  def job
+    @job
+  end
+
+  def name=(newname)
+    @name = newname
+  end
+
+  def job=(newjob)
+    @job = newjob
+  end
+
+end
