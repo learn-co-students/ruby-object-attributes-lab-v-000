94ca9ad42ee0b609349fb47cfd5c541e31c4d2b5
Bailey Sullivan <bailey.rose.sullivan@gmail.com>
2018-06-22 02:50:39 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,9 +1,15 @@
 class Dog 
-  def name=(dogs_name)
-    @this_dogs_name = dogs_name
+  def name=(name)
+    @name = name
   end
   def name
-   @this_dogs_name
+    @name
+  end
+  def breed=(breed)
+    @breed = breed
+  end
+  def breed
+    @breed
   end
 end
-    
+  
