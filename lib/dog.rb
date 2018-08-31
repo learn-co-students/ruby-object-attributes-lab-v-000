b97ce5db6bf040edc0595a937597b1f287dc3eef
Cheryl K <cherylkelson@icloud.com>
2018-08-31 16:10:44 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,16 @@
+class Dog
+  name = dog.new
+  def name = (dog_name)
+    @dogs_name
+  end
+  
+  def breed = (dogs_breed)
+    @dogs_breed = breed 
+  end
+  
+end
+  
+
+  
+  
+  
---
+++ lib/person.rb
@@ -0,0 +1,18 @@
+class Person
+  def name = (person_name)
+    @person_name = person_name
+  end 
+  def name 
+    @person_name
+end
+
+class Job 
+  def job = (person_job)
+    @person_job = person_job
+  end 
+  def job 
+    @peron_job
+  end 
+
+end
+
