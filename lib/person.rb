1be5725d7e22a8d13ef448f11709f542a99ef5e4
Bruce Hillsworth <bruce.hillsworth@gmail.com>
2018-09-01 18:21:23 +0000

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,18 @@
+class Person
+
+  def name=(the_persons_name)
+    @name = the_persons_name
+  end
+
+  def name
+    @name
+  end
+
+  def job=(the_persons_job)
+    @job = the_persons_job
+  end
+
+  def job
+    @job
+  end
+end
