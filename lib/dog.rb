8d58025817ee18927e718229307c74d9f81675f3
Kristen Robertson <kristen77@gmail.com>
2018-10-04 19:48:14 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -7,12 +7,12 @@ class Dog
     @breed
   end
     
- def name=(new_name)
-    @name = new_name
+ def name=(name)
+    @name = name
   end
   
- def breed=(new_breed)
-    @breed = new_breed
+ def breed=(breed)
+    @breed = breed
   end
 end
 
