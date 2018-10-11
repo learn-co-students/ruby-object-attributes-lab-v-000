0c1c8a20f6e33c69e58931d71a9751948b558808
Toni-Ty <antonique2479@yahoo.com>
2018-10-11 02:12:46 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,4 +1,31 @@
 class Dog
+
+  def name=(dog_name)
+    @name = dog_name
+  end
+
   def name
+    @name
+    end
+end
+
+class Breed
+
+def breed=(dog_breed)
+  @breed = dog_breed
+end
+
+def breed
+  @breed
   end
 end
+
+Fido = Dog.new
+Fido.name = "Fido"
+
+puts Fido.name
+
+Snoopy = Breed.new
+Snoopy.breed = "Beagle"
+
+puts Snoopy.breed
