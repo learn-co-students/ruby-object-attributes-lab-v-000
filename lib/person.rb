3f2be352ac458fb8078e6b4f13e595c17a488610
Bertha Muteba <nyc_bm26@aol.com>
2018-10-13 21:19:20 +0000

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,25 @@
+#!/usr/bin/env ruby
+
+class Person
+  def name=(name)
+    @name = name
+  end
+
+  def name
+    @name
+  end
+
+  def job=(job)
+    @job = job
+  end
+
+  def job
+    @job
+  end
+end
+
+beyonce = Person.new
+beyonce.name = "Beyonce"
+
+beyonce = Person.new
+beyonce.job = "Singer"
