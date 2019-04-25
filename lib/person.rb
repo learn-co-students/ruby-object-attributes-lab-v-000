c7673608dd676da0dc6c91d3866535a19ed60f14
BillyPittman1993 <billytrading93@gmail.com>
2019-04-25 18:27:13 +0000

    Done.

--- lib/person.rb
+++ lib/person.rb
@@ -1,17 +1,18 @@
 class Person 
+  
   def name=(name)
     @name = name 
   end 
+  
+  def job=(job)
+    @job = job
+  end
   
   def name 
     @name 
   end 
   
-  def job=(job)
-    @job = job 
-  end 
-  
-  def job 
+  def job
     @job 
   end 
 end 
