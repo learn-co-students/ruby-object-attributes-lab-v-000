1374e9d6742b1ff341718ab53fad97180c75cf49
Julie  McManus <julieannesays@gmail.com>
2020-01-30 20:22:56 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,18 +1,18 @@
 class Dog
-	def name=(dog_name)
-		@name = dog_name
+
+	def name=(dogs_name)
+		@name = dogs_name
 	end
 
-
-	def name 
+	def name
 		@name
 	end
 
 	def breed=(dogs_breed)
-		@breed = dogs_breed 
+		@breed = dogs_breed
 	end
 
-	def breed 
+	def breed
 		@breed
 	end
 end
