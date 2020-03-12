b84ab6099c9c9c0b8d08f31e988b71d5d0d839d7
Tom Sharkey <tbsharkey@gmail.com>
2020-03-12 00:36:31 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,14 +1,17 @@
 class Dog
-def name
-  @name
-end
-def name=(dog_name)
-  @name=dog_name
-end
-def breed
-  @breed
-end
-def breed=(type)
-  @breed=type
-end
+  def name
+    @name
+  end
+
+  def name=(name)
+    @name = name
+  end
+
+  def breed
+    @breed
+  end
+
+  def breed=(breed)
+    @breed = breed
+  end
 end
